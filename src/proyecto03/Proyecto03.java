04b43ec2ca4ffc2af193d24aaf6e3f843fdbb323
AlbornozSebastian1 <dj_biscacha@hotmail.com>
2024-05-11 01:24:20 -0500

    Primer avance del codigo fuente
    Primer avance del codigo fuente de un proyecto en java

---
+++ src/proyecto03/Proyecto03.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto03;
+
+/**
+ *
+ * @author dj_bi
+ */
+public class Proyecto03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo !!!!!!!");
+    }
+    
+}
